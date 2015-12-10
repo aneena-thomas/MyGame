d624528d461587e8d6090b56bab9ec5e1d60a903
aneena-thomas <Anu@Anu-MacBook-Pro.local>
2015-12-10 18:40:53 +0300

    secondcommit

--- MyGame/MyGame/Tile.swift
+++ MyGame/MyGame/Tile.swift
@@ -7,7 +7,16 @@
 //
 
 import UIKit
+import SpriteKit
 
 class Tile: SKSpriteNode {
 
+    
+    
+    var isAlive:Bool = false {
+        didSet {
+            self.hidden = !isAlive
+        }
+    }
+    var numLivingNeighbors = 0
 }
