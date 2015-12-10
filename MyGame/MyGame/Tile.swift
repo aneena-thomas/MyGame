2f9308e261f18072a10b56f1498c59594dab3ef3
aneena-thomas <Anu@Anu-MacBook-Pro.local>
2015-12-10 18:37:24 +0300

    second commit

---
+++ MyGame/MyGame/Tile.swift
@@ -0,0 +1,13 @@
+//
+//  Tile.swift
+//  MyGame
+//
+//  Created by Aneena Susan Thomas on 10/12/15.
+//  Copyright © 2015 Aneena Susan Thomas. All rights reserved.
+//
+
+import UIKit
+
+class Tile: SKSpriteNode {
+
+}
